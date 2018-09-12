fdae24b562ccfac1b8f72c57d745d4535d488a7b
Ian Paschal <ian.paschal@gmail.com>
2018-09-12 11:39:40 +0200

    Improved starting point

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import { render } from 'react-dom';
-import Home from './components/Home';
-import './App.css';
-
-render(
-	<Home></Home>,
-	document.getElementById('root')
-);
