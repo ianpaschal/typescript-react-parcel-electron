132dd676a62ee81a13cdd472a1daedb4302925b7
Ian Paschal <ian.paschal@gmail.com>
2018-09-12 08:15:38 +0200

    Initial files

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import { render } from 'react-dom';
+import Home from './components/Home';
+import './App.css';
+
+render(
+	<Home></Home>,
+	document.getElementById('root')
+);
